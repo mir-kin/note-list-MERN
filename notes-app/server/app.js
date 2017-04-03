5b865bd576843deff593946a7508d129945a5865
Anton Bashunov <abashunov@gmail.com>
2017-04-03 18:29:40 +0300

    Mirkin initial server setup

---
+++ notes-app/server/app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import { serverPort } from './config.json';
+
+import * as db from './utils/DataBaseUtils.js';
+
+db.setUpConnection();
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/notes', (req, res) => {
+    db.listNotes().then(data => res.send(data));
+});
+
+app.post('/notes', (req, res) => {
+    db.createNote(req.body).then(data => res.send(data));
+});
+
+app.delete('/notes/:id', (req, res) => {
+    db.deleteNote(req.params.id ).then(data => res.send(data));
+});
+
+const server = app.listen(serverPort, () => {
+   console.log(`Server is up and running ${serverPort} post`);
+});
---
+++ notes-app/server/utils/DataBaseUtils.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+import '../models/Note';
+
+import { db } from '../config.json';
+
+const Note = mongoose.model('Note');
+
+export function setUpConnection() {
+    mongoose.connect(`mongodb://${db.host}/${db.name}`);
+}
+
+export function listNotes() {
+    return Note.find();
+}
+
+export function createNote(data) {
+    const note = new Note({
+        title : data.title,
+        text : data.text,
+        color : data.color,
+        createdAt : new Date()
+    });
+
+    return note.save();
+}
+
+export function deleteNote(id) {
+    return Note.findById(id).remove();
+}
